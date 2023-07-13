8eba0afda7a8b0c870d2eb6740d721ab2cc35800
Ron Russo <rigorousron@gmail.com>
2023-07-13 19:57:04 -0400

    Committing excercise explanations

--- back_end/exercises/easy_1/even_numbers.js
+++ back_end/exercises/easy_1/even_numbers.js
@@ -6,3 +6,19 @@ for (let index = 1; index < 100; index += 1) {
     console.log(index);
   }
 }
+
+/*
+Explanation:
+
+first pass:
+On line 4, we declare a for loop with a loop variable `index` initialized to `1`
+. The condition of if the loop variable `index` is less than `100` will
+determine if the loop will continue to run. For each iteration of the loop, the
+`index` variable will be incremented by `1`. On line 5, an `if` uses the
+remainder operator `(%)` to check if the remainder of dividing the `index`
+variable by `2` is equal to `0`. This determines whether the `index` variable in
+the current iteration of the loop is even. On line 6, a call to the `log` method
+on the `console` object (`console.log`) is used inside of the if block to print
+out each even iteration of the `index` variable to the console, thus displaying
+all even numbers up to 100 (non-inclusive).
+*/
